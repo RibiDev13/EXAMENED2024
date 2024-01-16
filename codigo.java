70362462d05c2f40af4753306b7ce3db843776b3
Juan_Sanchez <juanitosanchezficticio@andorra.ad>
2024-01-16 14:14:00 +0100

    como cerré la consola vuelvo a repetir comandos

--- codigo.java
+++ codigo.java
@@ -1,6 +1,6 @@
 public class código {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
-        System.out.println("¿Cómo estás?");
+        System.out.println("¿Cómo estás?"); //cerré la consola y no pude copiar las instrucciones por eso salen ramas ya creadas.
     }
 }
